929eeb036347004747ca9a2249653b3ffc5dbe49
Julian Xhokaxhiu <jx@cron.eu>
2014-12-11 13:59:06 +0100

    Extend test.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp'),
-	usemin = require('./gulp-usemin-reloaded');
+	usemin = require('./gulp-usemin-reloaded'),
+	uglify = require('gulp-uglify'),
+	minifyHtml = require('gulp-minify-html'),
+	minifyCss = require('gulp-minify-css'),
+	rev = require('gulp-rev');
 
 gulp.task('test', function() {
 	return gulp
 	.src( 'test/in/*.html' )
 	.pipe(
 		usemin({
-			basePath: 'test/in/'
+			rules: {
+				build: {
+					css: [minifyCss(), 'concat'],
+					js: [uglify(), rev()],
+					html: [minifyHtml({empty: true})]
+				}
+			}
 		})
 	)
 	.pipe(
